efb0b9094154f25561e878d32cc4bf42d193c934
Noah Taiga Endo <nt.endo@hybrid-technologies.co.jp>
2024-06-12 18:33:20 +0900

    fixed a bug

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,7 +9,7 @@ const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
     amount: z.coerce.number(),
-    status: z.enum(['pending', 'pad']),
+    status: z.enum(['pending', 'paid']),
     date: z.string(),
 });
 
